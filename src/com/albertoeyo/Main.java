5175c5187af5f3dfe929fd53c8c6bb873ceeef30
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-08-23 11:59:09 +0200

    first commit
    first commit
    
    [FA-49]

---
+++ src/com/albertoeyo/Main.java
@@ -0,0 +1,22 @@
+package com.albertoeyo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args){
+        List<String> someBingoNumbers = Arrays.asList(
+                "N40", "N36",
+                "B12", "B6",
+                "G53", "G49", "G60", "G50",
+                "I26", "I17", "I29",
+                "O71"
+        );
+
+        someBingoNumbers.forEach(bingoNumber -> {
+            if (bingoNumber.startsWith("G")){
+                System.out.println(bingoNumber);
+            }
+        });
+    }
+}
