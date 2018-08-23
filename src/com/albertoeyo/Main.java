98656748afddb8eead671a333b2321a7639bc837
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-08-23 12:03:53 +0200

    Filter, sort and output
    Filter in a forEach, order and the print the numbers
    
    [FA-49]

--- src/com/albertoeyo/Main.java
+++ src/com/albertoeyo/Main.java
@@ -1,5 +1,6 @@
 package com.albertoeyo;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,15 +9,20 @@ public class Main {
         List<String> someBingoNumbers = Arrays.asList(
                 "N40", "N36",
                 "B12", "B6",
-                "G53", "G49", "G60", "G50",
+                "G53", "G49", "G60", "G50", "g64",
                 "I26", "I17", "I29",
                 "O71"
         );
 
+        final List<String> gNumbers = new ArrayList<>();
+
         someBingoNumbers.forEach(bingoNumber -> {
-            if (bingoNumber.startsWith("G")){
-                System.out.println(bingoNumber);
+            if (bingoNumber.toUpperCase().startsWith("G")){
+                gNumbers.add(bingoNumber);
             }
         });
+
+        gNumbers.sort((s1, s2) -> s1.compareTo(s2));
+        gNumbers.forEach(System.out::println);
     }
 }
